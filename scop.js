2f3cae4c84e87735bfd8cd13b4167e001529164a
rayhanabir <rayhanabir915@gmail.com>
2021-04-20 20:50:28 +0600

    scop

---
+++ scop.js
@@ -0,0 +1,21 @@
+
+let bonus  = 20; //eta global variable. function er vitore and bahire use kora jabe.
+function sum (first, second){
+    let result =  first + second + bonus ;//result variable k function er bahir theke call kora jabe nah. korle eror dibe.eta local variable .
+    // console.log(bonus);
+    if(result>9){
+        let mood = 'happy';
+        mood = 'fishy';
+        mood = 'funky';
+        console.log(mood);
+    }
+     console.log(mood);
+    return result;
+    
+
+}
+const output = sum(3, 7);
+// console.log(bonus);
+// console.log(output);
+
+// console.log(output);
